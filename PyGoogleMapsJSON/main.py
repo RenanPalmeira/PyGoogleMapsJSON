3e7924056cc3bc24c985728c99a296b576ca2e7a
RenanPalmeira <tomrpg44@gmail.com>
2014-04-01 12:17:01 -0300

    script main

--- PyGoogleMapsJSON/main.py
+++ PyGoogleMapsJSON/main.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- 
+import os
 import sys
 import json
 import urllib
 import StringIO
+from jinja2 import Environment, FileSystemLoader
 from wsgiref.simple_server import make_server
 
+folder = os.path.dirname(os.path.abspath(__file__))
+
 url=lambda name: "http://maps.google.com/maps/api/geocode/json?" + (urllib.urlencode({'address':(name),'sensor':'false'}))
+
+
+def template(**kwargs):
+    var=kwargs
+    j2_env = Environment(loader=FileSystemLoader(folder),trim_blocks=True)
+    return j2_env.get_template('template.html').render(var)
+    
 def GoogleMap(user):
     pattern=url(str(user))
     
@@ -22,23 +33,25 @@ def GoogleMap(user):
     else:
        lat=result['results'][0]['geometry']['location']['lat']
        lng=result['results'][0]['geometry']['location']['lng']
-       return str((lat,lng))
-    
-    
+       address=result['results'][0]['address_components'][0]['short_name']
+       return [lat,lng,address]
+
 if __name__=='__main__':
     host='localhost'
     port=5000
     
-    user=raw_input("Digite um endereço:")
-    address=GoogleMap(user)
+    user=raw_input("Digite Um Endereço:")
     
-    def app(env,res):
-        res('200 OK',[('Content-type','text/html')])
-        return address
+    if GoogleMap(user):
+        lat,lng,title=GoogleMap(user)
+        def app(env,res):
+            res('200 OK',[('Content-type','text/html')])
+            address=template(title=title,x=lat,y=lng)
+            return [address.encode('utf-8')]
+        
+        print "* Running http://%s:%s" % (host,port)
+        server=make_server(host,port,app)
+        server.serve_forever()
     
-    
-    print "* Running http://%s:%s" % (host,port)
-    
-    server=make_server(host,port,app)
-    server.handle_request()
-    
+    else:
+        print "Nenhum Endereço Encontrado"
